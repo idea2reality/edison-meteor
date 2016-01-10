b741958b9456257da7777a9bdb134b38a0a6fad6
Jun <i2r.jun@gmail.com>
2016-01-10 19:49:57 +0900

    Initial commit

---
+++ src/server/startup/loadDevices.ts
@@ -0,0 +1,36 @@
+Meteor.startup(function() {
+    if (Devices.find().count() === 0) {
+        var protocol = {
+            prefix: [2],
+            postfix: [3]
+        };
+
+        var edisons = [
+            {
+                '_id': 'cc3200',
+                'name': 'CC3200',
+                'avatar': 'svg-2',
+                'isAlive': false,
+                'leds': [
+                    { '_id': 1, 'type': 'cc3200', 'name': '거실', 'value': false },
+                    { '_id': 2, 'type': 'cc3200-dim', 'name': '안방', 'value': [30, 90, 50], 'protocol': protocol }
+                ]
+            },
+            {
+                '_id': 'my_edison',
+                'name': 'My Edison',
+                'avatar': 'svg-11',
+                'isAlive': false,
+                'leds': [
+                    { '_id': 'dim1', 'type': 'dim', 'name': '안방', 'value': 0 },
+                    { '_id': 'dim2', 'type': 'dim', 'name': '거실', 'value': 0 },
+                    { '_id': 'dim3', 'type': 'dim', 'name': '침실', 'value': 0 }
+                ]
+            }
+        ];
+
+        for (var i = 0; i < edisons.length; i++) {
+            Devices.insert(edisons[i]);
+        }
+    }
+});
